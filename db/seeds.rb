085ef857d006f59ac755bd122a21e7441d6ff425
alec wayman <aejw_edu@pm.me>
2021-02-16 16:24:15 +0000

    added CRUD and htmls

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# no_of_entries = 10
+
+# no_of_entries.times {
+#   a = Article.new(title: "#{Faker::Hacker.abbreviation} in #{Faker::Hacker.ingverb} #{Faker::Hacker.adjective} #{Faker::Hacker.noun}", content: Faker::Hacker.say_something_smart)
+#   a.save
+# }
+
+a = Article.new(title: "Hello, darkness", content: ", my old firend...")
+a.save
